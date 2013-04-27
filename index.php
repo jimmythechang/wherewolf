249c227659ac7d47b01d95893c98f1d4bb578f0b
Jimmy Chang <jimmythechang@gmail.com>
2013-04-26 23:02:35 -0500

    Added require.js magic.

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <title></title>
+        <script data-main="js/Wherewolf" src="js/require-jquery.js"></script>
     </head>
     <body>
         <canvas id="canvas" height="600" width="800" style="border: 1px solid #000">
